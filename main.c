963af6ef95095640e090acfcb9f2fd749dd71b4f
Jesu Lacastesantos <jejolac@gmail.com>
2021-12-18 18:59:03 +0800

    special char removal and added bugfixes for add_item() function

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ void main() {
 				break;
 			case 'D':
 				//search for an inventory item
-				//search_item();
+				search_item();
 				break;
 			case 'X':
 				printf("\nThank you for using the program!\nProgram now closing.");
@@ -48,5 +48,6 @@ void main() {
 				system("pause");
 				break;                                                   
 		}
+		fflush(stdin);
 	} 
 }
